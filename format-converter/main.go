a2167a34fa9b27374f47837b142ea29afdf95fc9
Matt Aimonetti <mattaimonetti@gmail.com>
2017-02-17 19:13:36 -0800

    switched the format converter to use chunks

--- format-converter/main.go
+++ format-converter/main.go
@@ -10,11 +10,12 @@ import (
 	"github.com/go-audio/aiff"
 	"github.com/go-audio/audio"
 	"github.com/go-audio/wav"
+	"github.com/mattetti/audio/decoder"
 )
 
 var (
 	flagInput  = flag.String("input", "", "The file to convert")
-	flagFormat = flag.String("format", "aiff", "The format to convert to (wav or aiff)")
+	flagFormat = flag.String("format", "wav", "The format to convert to (wav or aiff)")
 	flagOutput = flag.String("output", "out", "The output filename")
 )
 
@@ -39,10 +40,7 @@ func main() {
 	}
 	defer f.Close()
 
-	type decoder interface {
-		FullPCMBuffer() (*audio.IntBuffer, error)
-	}
-	var dec decoder
+	var dec decoder.Decoder
 
 	type encoder interface {
 		Write(b *audio.IntBuffer) error
@@ -51,49 +49,81 @@ func main() {
 	var enc encoder
 
 	var buf *audio.IntBuffer
-	var bitDepth int
 
 	wd := wav.NewDecoder(f)
 	if wd.IsValidFile() {
-		bitDepth = int(wd.BitDepth)
 		dec = wd
 	} else {
 		f.Seek(0, 0)
 		aiffd := aiff.NewDecoder(f)
-		if aiffd.IsValidFile() {
-			bitDepth = int(wd.BitDepth)
+		if !aiffd.IsValidFile() {
+			fmt.Println("input file isn't a valid wav or aiff file")
+			os.Exit(1)
 		}
 		dec = aiffd
 	}
 
-	// TODO: switch to encode/decode in chunks
-	buf, err = dec.FullPCMBuffer()
-	if err != nil {
-		panic(err)
+	if !dec.WasPCMAccessed() {
+		err := dec.FwdToPCM()
+		if err != nil {
+			panic(err)
+		}
 	}
 
+	format := dec.Format()
+
+	var of *os.File
 	outputFilename := fmt.Sprintf("%s.%s", *flagOutput, *flagFormat)
-	of, err := os.Create(outputFilename)
-	if err != nil {
-		panic(err)
+	switch *flagFormat {
+	case "wav", ".wav", "wave":
+		of, err = os.Create(outputFilename)
+		if err != nil {
+			panic(err)
+		}
+		defer of.Close()
+		enc = wav.NewEncoder(of, format.SampleRate, int(dec.SampleBitDepth()), format.NumChannels, 1)
+	case "aif", ".aif", "aiff", ".aiff":
+		of, err = os.Create(outputFilename)
+		if err != nil {
+			panic(err)
+		}
+		defer of.Close()
+		enc = aiff.NewEncoder(of, format.SampleRate, int(dec.SampleBitDepth()), format.NumChannels)
+	default:
+		fmt.Printf("output format %s not supported\n", *flagFormat)
+		os.Exit(1)
 	}
 
-	fmt.Println("File format", buf.Format.SampleRate, bitDepth, buf.Format.NumChannels)
+	buf = &audio.IntBuffer{Format: format, Data: make([]int, 4096)}
 
-	if *flagFormat == "aiff" {
-		enc = aiff.NewEncoder(of, buf.Format.SampleRate, bitDepth, buf.Format.NumChannels)
-	} else {
-		enc = wav.NewEncoder(of,
-			buf.Format.SampleRate,
-			bitDepth,
-			buf.Format.NumChannels,
-			1)
+	var t int64
+	var n int
+	for err == nil && t <= dec.PCMLen() {
+		n, err = dec.PCMBuffer(buf)
+		if err != nil {
+			fmt.Println("failed to read the input file -", err)
+			os.Exit(1)
+		}
+		t += int64(n)
+		if n == 0 {
+			break
+		}
+		if n != len(buf.Data) {
+			buf.Data = buf.Data[:n]
+		}
+		if err = enc.Write(buf); err != nil {
+			fmt.Println("failed to write to the output file -", err)
+			os.Exit(1)
+		}
+		if n != len(buf.Data) {
+			break
+		}
 	}
 
-	if err := enc.Write(buf); err != nil {
-		panic(err)
+	if err = enc.Close(); err != nil {
+		fmt.Println("failed to close the encoder stream")
+		os.Exit(1)
 	}
-	enc.Close()
 	of.Close()
 	fmt.Println(outputFilename, "written to disk")
 }
